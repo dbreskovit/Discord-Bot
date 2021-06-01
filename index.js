9b89f076270185e39cd779abf22ee961ca9dc05f
Diego Breskovit <81165451+dbreskovit@users.noreply.github.com>
2021-06-01 19:51:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js'); 
+const client = new Discord.Client(); 
+const {prefix,token} = require('./config.json'); 
+
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.channel.type == 'dm') return;
+    if (!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
+    if (message.content.startsWith(`<@!${client.user.id}>`) || 
+    message.content.startsWith(`<@${client.user.id}>`)) return;
+
+    const args = message.content
+        .trim().slice(prefix.length)
+        .split(/ +/g);
+
+    const command = args.shift().toLowerCase();
+
+    try 
+        {const commandFile = require(`./commands/${command}.js`)
+        commandFile.run(client, message, args);}
+
+    catch(err) 
+        {console.error('Erro:' + err);}});
+
+
+client.login(token);
